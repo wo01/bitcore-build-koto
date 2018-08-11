426f86cdb44e998c49b48bd2f89652472c4f1acb
WO <wo@kotocoin.info>
2018-08-11 12:26:04 +0900

    Bug Fix

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function startGulp(name, opts) {
 
   var buildPath = './node_modules/bitcore-build-koto/';
   var buildModulesPath = buildPath + 'node_modules/';
-  var buildBinPath = buildPath + 'node_modules/.bin/';
+  var buildBinPath = './node_modules/.bin/';
 
   /**
    * testing
